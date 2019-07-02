dd1b7a7e64f6be7b3c901f9f9964b816abccaa0b
salwa-essid <salwa.essid@gmail.com>
2019-07-02 17:45:30 +0200

    anim js

--- script.js
+++ script.js
@@ -6,57 +6,58 @@ $(document).ready(function(){
     
 
 
- //toggler btn
-$('.navbar-toggler').click(function(){
-  $('.navbar-toggler').toggleClass('change')
-})
-
-
-/*//sticky navbar less paddin
-$('window').scroll(function(){
-  let position=$(this).scrollTop();
-  if(position >= 718){
-    $('.navbar').addClass('navbar-background');
-    $('.navbar').addClass('fixed-top');
-  }
-   else{
-    $('.navbar').removeClass('navbar-background');
-    $('.navbar').removeClass('fixed-top');
-  }
-
-})*/
-
-//smooth scroll
-$('.nav-item a, .header-link,#back-to-top').click(function(link){
-  link.preventDefault();
-  let target=$(this).attr('href');
-  $('html,body').animate({scrollTop: $(target).offset().top -25
-  },3000);
-})
-
-
-
-
-  //ripples
-
-    $("#header, .navbar").ripples({
-        dropRadius:25,
-        perturbance: 0.6,
-        
-      });
-      //magnific popup
-      $('.parent-container').magnificPopup({
-        delegate: 'a', // child items selector, by clicking on it popup will open
-        type: 'image',
-        gallery:{
-          enabled:true}
-        
-        // other options
-      }); 
-});
-
-
-
-
-
-
+  //toggler btn
+ $('.navbar-toggler').click(function(){
+   $('.navbar-toggler').toggleClass('change')
+ })
+ 
+ 
+ /*//sticky navbar less paddin
+ $('window').scroll(function(){
+   let position=$(this).scrollTop();
+   if(position >= 718){
+     $('.navbar').addClass('navbar-background');
+     $('.navbar').addClass('fixed-top');
+   }
+    else{
+     $('.navbar').removeClass('navbar-background');
+     $('.navbar').removeClass('fixed-top');
+   }
+ 
+ })*/
+ 
+ //smooth scroll
+ $('.nav-item a, .header-link,#back-to-top').click(function(link){
+   link.preventDefault();
+   let target=$(this).attr('href');
+   $('html,body').animate({scrollTop: $(target).offset().top -25
+   },3000);
+ })
+ 
+ 
+ 
+ 
+   //ripples
+ 
+     $("#header, .navbar").ripples({
+         dropRadius:25,
+         perturbance: 0.6,
+         
+       });
+       //magnific popup
+       $('.parent-container').magnificPopup({
+         delegate: 'a', // child items selector, by clicking on it popup will open
+         type: 'image',
+         gallery:{
+           enabled:true}
+         
+         // other options
+       }); 
+ });
+ 
+ 
+ 
+ 
+ 
+ 
+ 
